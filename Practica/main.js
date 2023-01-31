318759f2b409671e3298801da33d08836f7b22a9
Melvin565484 <87821610+Melvin565484@users.noreply.github.com>
2023-01-31 01:07:37 -0300

    Add files via upload

---
+++ Practica/main.js
@@ -0,0 +1,30 @@
+//constantes del boton ocultar y el div
+const hidebtn = document.querySelector('#hideElements')
+const listdiv = document.querySelector('#list')
+
+
+
+
+//agrega evento al boton hidebtn
+hidebtn.addEventListener('click', ()=>{
+    //alert('ola que ase')
+    if(listdiv.style.display == 'none'){
+        listdiv.style.display = 'block'
+        hidebtn.textContent = 'Ocultar'
+    } else {
+        listdiv.style.display = 'none'
+        hidebtn.textContent = 'Mostrar'
+    }
+})
+
+//agregar elementos a la lista
+const addItemInput = document.querySelector('#addItem')
+const addItemButton = document.querySelector('button#addItemButton')
+
+addItemButton.addEventListener('click', () =>{
+    let list = document.querySelector('ul')
+    let li = document.createElement('li')
+    li.textContent = addItemInput.value
+    list.appendChild(li)
+
+}) 
